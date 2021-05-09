5bd47d3208c7fa1cde429034f5e8902deb9024b4
prasannavenkat-dev <74189369+prasannavenkat-dev@users.noreply.github.com>
2021-05-09 11:40:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ console.log(process.env.MAIL)
 
 
                 } else {
-                  res.redirect('https://prasannavenkatesh.netlify.app/#success')
+                  res.redirect('https://www.google.com/')
                 }
 
 
