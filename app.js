b98215a2d99cc34a3657f3c0c7b9fe225db20340
prasannavenkat-dev <74189369+prasannavenkat-dev@users.noreply.github.com>
2021-05-10 22:41:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -27,14 +27,6 @@ app.use((req, res, next) => {
     next();
   });
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin","*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  });
 
 
 
